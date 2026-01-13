6054d242e1c19bbde8cf2a3cb844a283fe420ee2
Home <tuptidu13042011@gmail.com>
2026-01-13 14:24:19 +0300

    Fix backend config

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 
 export const http = axios.create({
-<<<<<<< HEAD
+
   baseURL: "https://rooms-2-ncqy.onrender.com/api/rooms?page=1",
-=======
-   baseURL: 'https://rooms-2-ncqy.onrender.com/api/rooms?page=1',
->>>>>>> c5eee91a75139250144d9e77c052a285d10c0d42
   timeout: 10_000,
   headers: { "Content-Type": "application/json" },
 });
